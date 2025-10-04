f032f542ffcaea9057656e6edf1bb6e548c6cfec
maximeDzN <maxime.Dazin@gmail.com>
2025-10-04 12:39:49 +0200

    Update swagger

--- src/main/java/fr/dazin/maxime/itimdb/web/controller/MovieStatusController.java
+++ src/main/java/fr/dazin/maxime/itimdb/web/controller/MovieStatusController.java
@@ -3,6 +3,7 @@ package fr.dazin.maxime.itimdb.web.controller;
 import fr.dazin.maxime.itimdb.service.MovieStatusService;
 import fr.dazin.maxime.itimdb.web.dto.MovieWithStatusDto;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,20 +19,27 @@ public class MovieStatusController {
         this.movieStatusService = movieStatusService;
     }
 
-    @GetMapping( "favorites")
-    public List<MovieWithStatusDto> listFavorites(@Parameter(example = "1") @PathVariable long userId, @RequestParam(defaultValue = "releaseDate") String sortBy, @RequestParam(defaultValue = "asc") String order) {
-        return this.movieStatusService.listFavorites(userId,sortBy,order);
+    @GetMapping("favorites")
+    public List<MovieWithStatusDto> listFavorites(@Parameter(example = "1") @PathVariable long userId, @Parameter(
+            example = "releaseDate",
+            description = "Champ utilisé pour trier les films",
+            schema = @Schema(allowableValues = {"releaseDate", "rating"})
+    ) @RequestParam(defaultValue = "releaseDate") String sortBy,@Parameter(example = "asc",
+            description = "Ordre de tri",
+            schema = @Schema(allowableValues = {"asc", "desc"})
+    )  @RequestParam(defaultValue = "asc") String order) {
+        return this.movieStatusService.listFavorites(userId, sortBy, order);
     }
 
     @PutMapping("/{movieId}/favorite")
-    public ResponseEntity<Void> updateFavorite(@Parameter(example = "1") @PathVariable long userId, @Parameter(example = "10")@PathVariable long movieId, @RequestBody boolean favorite) {
-        this.movieStatusService.updateFavorite(userId,movieId,favorite);
+    public ResponseEntity<Void> updateFavorite(@Parameter(example = "1") @PathVariable long userId, @Parameter(example = "10") @PathVariable long movieId, @RequestBody boolean favorite) {
+        this.movieStatusService.updateFavorite(userId, movieId, favorite);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{movieId}/seen")
     public ResponseEntity<Void> updateSeen(@Parameter(example = "1") @PathVariable long userId, @Parameter(example = "10") @PathVariable long movieId) {
-        this.movieStatusService.updateSeen(userId,movieId);
+        this.movieStatusService.updateSeen(userId, movieId);
         return ResponseEntity.noContent().build();
     }
 
@@ -39,12 +47,11 @@ public class MovieStatusController {
     public List<MovieWithStatusDto> listSeen(@Parameter(example = "1") @PathVariable long userId) {
         return this.movieStatusService.listSeenMovies(userId);
     }
+
     @GetMapping("/unseen")
     public List<MovieWithStatusDto> listUnseen(@Parameter(example = "1") @PathVariable long userId) {
         return this.movieStatusService.listUnseenMovies(userId);
     }
-
-
 
 
 }
